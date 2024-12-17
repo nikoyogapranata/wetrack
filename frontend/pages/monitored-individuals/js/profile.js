d90d114ba1b63587135c42e19a0fbfa9ece1f810
nikoyogapranata <nikoyogapranata@gmail.com>
2024-12-17 14:49:58 +0700

    updating landing page

---
+++ frontend/pages/monitored-individuals/js/profile.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const viewReportBtn = document.getElementById('viewReportBtn');
+  
+  viewReportBtn.addEventListener('click', function() {
+      window.location.href = 'final-report.html';
+  });
+});
