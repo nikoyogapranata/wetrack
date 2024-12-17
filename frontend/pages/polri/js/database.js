e5a44339efc5bd1590321bf8168e6b3ce651d0b2
ardeliaptr <ardelialan1312@gmail.com>
2024-12-17 14:46:49 +0700

    page polri

---
+++ frontend/pages/polri/js/database.js
@@ -0,0 +1,8 @@
+function navigateTo(page) {
+    window.location.href = page;
+  }
+
+  function logout() {
+    alert("You have logged out.");
+    window.location.href = 'login.html'; // Redirect to login page
+  }
---
+++ frontend/pages/polri/js/profile.js
@@ -0,0 +1,9 @@
+function navigateTo(page) {
+    window.location.href = page;
+  }
+
+  function logout() {
+    alert("You have logged out.");
+    window.location.href = 'login.html'; // Redirect to login page
+  }
+  
