386141a03f5c7ac9654cb5540834a6f2d1608392
nikoyogapranata <nikoyogapranata@gmail.com>
2024-12-19 13:50:44 +0700

    polri awikwok

--- polri/pages/connection.php
+++ polri/pages/connection.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "root";
+$password = "";
 $dbname = "wetrack";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
