f7088d69ebf66cdcc80233b4960d1f89d0026352
ardeliaptr <ardelialan1312@gmail.com>
2024-12-19 13:40:01 +0700

    ganti sql terbaru

--- wetrack.sql
+++ wetrack.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Dec 19, 2024 at 04:10 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:8889
+-- Waktu pembuatan: 19 Des 2024 pada 06.39
+-- Versi server: 8.0.35
+-- Versi PHP: 8.2.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,28 +24,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `mantan_narapidana`
+-- Struktur dari tabel `data_polri`
+--
+
+CREATE TABLE `data_polri` (
+  `id` int NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nik` varchar(16) NOT NULL,
+  `id_napi` varchar(20) NOT NULL,
+  `alamat` text NOT NULL,
+  `unggah_foto` varchar(255) NOT NULL,
+  `isi_laporan` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `tanggal_laporan` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data untuk tabel `data_polri`
+--
+
+INSERT INTO `data_polri` (`id`, `nama`, `nik`, `id_napi`, `alamat`, `unggah_foto`, `isi_laporan`, `created_at`, `tanggal_laporan`) VALUES
+(1, 'nama', '1111111111111111', '123', 'qwe', 'uploads/6763baf87d618.jpeg', 'qwe', '2024-12-19 06:19:36', '2025-01-02'),
+(2, 'niko', '1111111111111111', '234', 'ruko', 'uploads/6763bb41314c0.jpeg', 'weok', '2024-12-19 06:20:49', '2024-12-18'),
+(3, 'rafi', '1111111111111111', '123', 'qwe', 'uploads/6763bcb4c93e1.jpeg', 'qwe', '2024-12-19 06:27:00', '2024-12-20'),
+(4, 'nama', '1111111111111111', 'qwe', 'qwe', 'uploads/6763bedb289fb.jpeg', 'qwe', '2024-12-19 06:36:11', '2024-12-14');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mantan_narapidana`
 --
 
 CREATE TABLE `mantan_narapidana` (
-  `id` int(11) NOT NULL,
-  `fileInput` varchar(255) DEFAULT NULL,
-  `nik` varchar(20) DEFAULT NULL,
-  `nrt` varchar(20) DEFAULT NULL,
-  `nama` varchar(100) DEFAULT NULL,
+  `id` int NOT NULL,
+  `fileInput` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nik` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nrt` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nama` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `dateBirth` date DEFAULT NULL,
-  `address` text DEFAULT NULL,
-  `gender` enum('male','female') DEFAULT NULL,
-  `nationality` varchar(100) DEFAULT NULL,
-  `crime` enum('TwA','Ot','Fraud','Assault','NO','Embezzlement','MvT','Robbery','Brawling') DEFAULT NULL,
-  `case` text DEFAULT NULL,
-  `punishment` varchar(100) DEFAULT NULL,
+  `address` text COLLATE utf8mb4_general_ci,
+  `gender` enum('male','female') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `nationality` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `crime` enum('TwA','Ot','Fraud','Assault','NO','Embezzlement','MvT','Robbery','Brawling') COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `case` text COLLATE utf8mb4_general_ci,
+  `punishment` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `releaseDate` date DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `mantan_narapidana`
+-- Dumping data untuk tabel `mantan_narapidana`
 --
 
 INSERT INTO `mantan_narapidana` (`id`, `fileInput`, `nik`, `nrt`, `nama`, `dateBirth`, `address`, `gender`, `nationality`, `crime`, `case`, `punishment`, `releaseDate`, `created_at`) VALUES
@@ -59,20 +87,32 @@ INSERT INTO `mantan_narapidana` (`id`, `fileInput`, `nik`, `nrt`, `nama`, `dateB
 --
 
 --
--- Indexes for table `mantan_narapidana`
+-- Indeks untuk tabel `data_polri`
+--
+ALTER TABLE `data_polri`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `mantan_narapidana`
 --
 ALTER TABLE `mantan_narapidana`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `mantan_narapidana`
+-- AUTO_INCREMENT untuk tabel `data_polri`
+--
+ALTER TABLE `data_polri`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `mantan_narapidana`
 --
 ALTER TABLE `mantan_narapidana`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
