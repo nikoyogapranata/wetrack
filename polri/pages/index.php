7fa0737e34dc94a4b3a7a2cbc6f7acac00e6555e
nikoyogapranata <nikoyogapranata@gmail.com>
2024-12-19 10:43:14 +0700

    update path css polri index

--- polri/pages/index.php
+++ polri/pages/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>POLRI Admin Dashboard</title>
-  <link rel="stylesheet" href="/frontend/pages/polri/css/styles.css">
+  <link rel="stylesheet" href="http://localhost/wetrack/polri/css/styles.css">
   <!-- Tambahkan Chart.js Library -->
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
